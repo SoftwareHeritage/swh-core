dfff2af4b707ff3fafdef0442c33451ce553b2b5
Stefano Zacchiroli <zack@upsilon.cc>
2015-09-16 16:36:23 +0200

    test_config.py: flake8 fixes

--- swh/core/tests/test_config.py
+++ swh/core/tests/test_config.py
@@ -14,10 +14,9 @@ from swh.core import config
 
 
 def prepare_dummy_conf_file(tmp_dir):
-    tmp_conf_file = tempfile.NamedTemporaryFile(mode='w',
-                                                suffix="swh-core-test-read-conf",
-                                                dir=tmp_dir,
-                                                delete=False)
+    tmp_conf_file = tempfile.NamedTemporaryFile(
+        mode='w', suffix="swh-core-test-read-conf",
+        dir=tmp_dir, delete=False)
     with open(tmp_conf_file.name, 'w') as f:
         f.write("""[main]
 a = 1
@@ -32,8 +31,8 @@ class ConfReaderTest(unittest.TestCase):
     @classmethod
     def setUp(self):
         # create a temporary folder
-        self.tmp_work_folder = tempfile.mkdtemp(prefix='test-swh-core.',
-                                               dir='/tmp')
+        self.tmp_work_folder = tempfile.mkdtemp(
+            prefix='test-swh-core.', dir='/tmp')
 
         self.tmp_conf_file = prepare_dummy_conf_file(self.tmp_work_folder)
 
@@ -65,18 +64,24 @@ class ConfReaderTest(unittest.TestCase):
                 'path2': self.tmp_work_folder + 'path2/depth1'}
 
         # the folders does not exists
-        self.assertFalse(os.path.exists(conf['path1']), "path1 should not exist.")
-        self.assertFalse(os.path.exists(conf['path2']), "path2 should not exist.")
+        self.assertFalse(os.path.exists(conf['path1']),
+                         "path1 should not exist.")
+        self.assertFalse(os.path.exists(conf['path2']),
+                         "path2 should not exist.")
 
         # when
         config.prepare_folders(conf, 'path1')
 
         # path1 exists but not path2
-        self.assertTrue(os.path.exists(conf['path1']), "path1 should now exist!")
-        self.assertFalse(os.path.exists(conf['path2']), "path2 should not exist.")
+        self.assertTrue(os.path.exists(conf['path1']),
+                        "path1 should now exist!")
+        self.assertFalse(os.path.exists(conf['path2']),
+                         "path2 should not exist.")
 
         # path1 already exists, skips it but creates path2
         config.prepare_folders(conf, 'path1', 'path2')
 
-        self.assertTrue(os.path.exists(conf['path1']), "path1 should still exist!")
-        self.assertTrue(os.path.exists(conf['path2']), "path2 should now exist.")
+        self.assertTrue(os.path.exists(conf['path1']),
+                        "path1 should still exist!")
+        self.assertTrue(os.path.exists(conf['path2']),
+                        "path2 should now exist.")
