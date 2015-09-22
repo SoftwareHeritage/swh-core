5dd900a31081dcdaec1a0fe7274a6b34ef25939a
Nicolas Dandrimont <nicolas@dandrimont.eu>
2015-09-22 14:45:40 +0200

    setup.py: Make requirements.txt parsing more robust

--- setup.py
+++ setup.py
@@ -2,6 +2,19 @@
 
 from setuptools import setup
 
+
+def parse_requirements():
+    requirements = []
+    with open('requirements.txt') as f:
+        for line in f.readlines():
+            line = line.strip()
+            if not line or line.startswith('#'):
+                continue
+            requirements.append(line)
+
+    return requirements
+
+
 setup(
     name='swh.core',
     description='Software Heritage core utilities',
@@ -10,7 +23,7 @@ setup(
     url='https://forge.softwareheritage.org/diffusion/DCORE/',
     packages=['swh.core', 'swh.core.tests'],
     scripts=['bin/swh-hashdir', 'bin/swh-hashfile'],
-    install_requires=open('requirements.txt').read().splitlines(),
+    install_requires=parse_requirements(),
     setup_requires=['vcversioner'],
     vcversioner={},
     include_package_data=True,
