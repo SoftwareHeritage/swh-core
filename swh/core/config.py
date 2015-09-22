49ec69aef8ab7ac678e6895cfc4adfcc8ae0c604
Stefano Zacchiroli <zack@upsilon.cc>
2015-09-22 13:47:00 +0200

    config.py: remove unneeded python3 shebang line

--- swh/core/config.py
+++ swh/core/config.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 # Copyright (C) 2015  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU General Public License version 3, or any later version
