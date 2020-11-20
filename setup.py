7722df9f290aefeb4f5d822578719052922739bf
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2020-11-20 13:52:22 +0100

    setup: Separate pytest-postgresql dependency and declare it when needed
    This drops the pytest-postgresql from requirements-db.txt. This is not a
    required dependency for the swh.core.db module itself.
    
    It's a requirement for the swh.core.db.pytest_plugin. This module should become
    its own module as a test requirements for other swh modules (including
    swh.core.db but not limited to it).
    
    So this commit adds a dedicated requirements-db-pytestplugin.txt file with such
    dependency. This opens the db-pytestplugin key within the setup.py and then
    references that test dependency for the swh.core.db modules (as it's needed for
    tests of the module).
    
    Related to T2746

--- setup.py
+++ setup.py
@@ -55,11 +55,13 @@ setup(
     extras_require={
         "testing-core": parse_requirements("test"),
         "logging": parse_requirements("logging"),
-        "db": parse_requirements("db"),
+        "db": parse_requirements("db", "db-pytestplugin"),
         "testing-db": parse_requirements("test-db"),
         "http": parse_requirements("http"),
         # kitchen sink, please do not use
-        "testing": parse_requirements("test", "test-db", "db", "http", "logging"),
+        "testing": parse_requirements(
+            "test", "test-db", "db", "db-pytestplugin", "http", "logging"
+        ),
     },
     include_package_data=True,
     entry_points="""
